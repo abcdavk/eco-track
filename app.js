461040509163775f3f6bcd69d6a950323815d9fb
abcdavk <xloot.mcpedl@gmail.com>
2026-04-27 13:30:56 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files dari folder public
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+
+app.listen(3000, () => {
+  console.log('Server berjalan di http://localhost:3000');
+});
+
+app.post('/calculate', (req, res) => {
+  res.json({
+    success: true,
+    message: "Calculation success!!"
+  });
+});
