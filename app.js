2389ff42050e5dd32b3d4fe5680c47d7e1fa7833
abcdavk <xloot.mcpedl@gmail.com>
2026-04-28 19:03:52 +0700

    add /dashboard route to public/page/dashboard.html

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+
 // Serve static files dari folder public
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.json());
-
-app.listen(3000, () => {
-  console.log('Server berjalan di http://localhost:3000');
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/page/dashboard.html'))
 });
 
 app.post('/calculate', (req, res) => { 
@@ -17,4 +17,8 @@ app.post('/calculate', (req, res) => {
     success: true,
     message: "Calculation success!!"
   });
+});
+
+app.listen(3000, () => {
+  console.log('Server berjalan di http://localhost:3000');
 });
