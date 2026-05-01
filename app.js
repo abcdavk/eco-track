73e5c0ff216d6023c065985911f4a1b5a0f35d8d
abcdavk <xloot.mcpedl@gmail.com>
2026-05-01 14:07:32 +0700

    add a dashboard hero section and bento

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const app = express();
+const app = express(); 
 
 app.use(express.json());
 
@@ -12,8 +12,16 @@ app.get('/dashboard', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'page', 'dashboard.html'));
 });
 
-app.post('/calculate', (req, res) => { 
-  res.json({
+app.post('/calculate', (req, res) => {
+  console.log(req.body)
+  res.status(200).json({
+    success: true,
+    message: "Calculation success!!"
+  });
+});
+
+app.get('/calculate', (req, res) => {
+  res.status(200).json({
     success: true,
     message: "Calculation success!!"
   });
