09363f73e50623beffc6afc20a538ef46be404e3
abcdavk <xloot.mcpedl@gmail.com>
2026-05-02 16:57:51 +0700

    vehicle selector is now working

--- public/scripts/calculator.js
+++ public/scripts/calculator.js
@@ -14,36 +14,82 @@ function initCalculator() {
   const progressBarFill = document.getElementById('progressBarFill');
   const dottedCircle = document.getElementById('dottedCircle');
   const middleEmissionText = document.getElementById('middleEmissionText');
+  const motorIcon = document.getElementById('motorIcon');
+  const carIcon = document.getElementById('carIcon');
 
-  if (!calculateBtn) return;
-  if (!distanceTravel) return;
-  if (!acHours) return;
-  if (!laptopHours) return;
-  if (!visualizer) return;
+  const motorBtn = document.getElementById('motorBtn');
+  const carBtn = document.getElementById('carBtn');
+  
+  if (!calculateBtn || !distanceTravel ||
+    !acHours || !laptopHours ||
+    !visualizer || !progressText ||
+    !progressBarFill || !dottedCircle ||
+    !middleEmissionText || !motorBtn ||
+    !carBtn || !motorIcon || !motorIcon
+  ) return;
 
   distanceTravel.addEventListener('input', () => formInputUpdate());
   acHours.addEventListener('input', () => formInputUpdate());
   laptopHours.addEventListener('input', () => formInputUpdate());
 
-  function formInputUpdate() {
-    const hueRed = 150;
-    const hueGreen = 280;
+  let vehicle_type = 'motor';
+  motorBtn.addEventListener('click', () => toggleBtnMotor())
+  carBtn.addEventListener('click', () => toggleBtnCar())
 
-    let emission = countEmission(distanceTravel.value, "motor", acHours.value, laptopHours.value);
-    emission = emission.toFixed(2)
+  function toggleBtnMotor() {
+    vehicle_type = 'motor';
+    carBtn.style.backgroundColor = `var(--bg-100)`;
+    carBtn.style.color = `var(--text)`;
+    carIcon.style.fill = `var(--text)`;
+    
+    motorBtn.style.backgroundColor = `var(--green-400)`;
+    motorBtn.style.color = `var(--text-white)`;
+    motorIcon.style.fill = `var(--text-white)`;
+    formInputUpdate()
+  }
+  function toggleBtnCar() {
+    vehicle_type = 'car';
+    motorBtn.style.backgroundColor = `var(--bg-100)`;
+    motorBtn.style.color = `var(--text)`;
+    motorIcon.style.fill = `var(--text)`;
+  
+    carBtn.style.backgroundColor = `var(--green-400)`;
+    carBtn.style.color = `var(--text-white)`;
+    carIcon.style.fill = `var(--text-white)`;
+    formInputUpdate();
+  }
+
+  function formInputUpdate() {
+    let emission = countEmission(distanceTravel.value, vehicle_type, acHours.value, laptopHours.value);
+    emission = emission.toFixed(2);
 
     const percentage = Math.min(100, Math.max(0, (emission/max_daily_emission) * 100));
     const t = percentage/100;
-    const hue = hueGreen + (hueRed - hueGreen) * t;
     const rotation = 360 * t;
+    let hue;
+    
+    if (emission <= 2) {
+      hue = 280;
+    } else if (emission <= 5) {
+      hue = 200;
+    } else {
+      hue = 130;
+    }
 
     progressBarFill.style.width = `${percentage}%`;
     progressText.innerHTML = `${emission} KG CO2e`;
-    visualizer.style.filter = `hue-rotate(${hue}deg)`;
 
-    dottedCircle.style.transform = `rotate(${rotation}deg)`
-    middleEmissionText.innerHTML = `${percentage.toFixed()}%`
+    const intensity = emission > 5 ? 0.2 : 0;
+    visualizer.style.backgroundImage = `radial-gradient(rgba(255,255,255,0.7), rgba(0,68,255,${intensity})), url("../../img/your_impact.webp")`;
+
+    const saturate = emission > 5 ? " saturate(300%)" : "";
+    visualizer.style.filter = `hue-rotate(${hue}deg)${saturate}`;
+
+    dottedCircle.style.transform = `rotate(${rotation}deg)`;
+    middleEmissionText.innerHTML = `${percentage.toFixed()}%`;
   }
+  toggleBtnMotor();
+  formInputUpdate();
 }
 
 function countEmission(distance = 0, vehicle_type = "motor", ac_dur = 0, laptop_dur = 0) {
@@ -53,4 +99,4 @@ function countEmission(distance = 0, vehicle_type = "motor", ac_dur = 0, laptop_
 
 window.addEventListener('DOMContentLoaded', () => {
   initCalculator();
-})
+});
